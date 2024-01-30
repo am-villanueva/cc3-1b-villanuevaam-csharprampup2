1ca8848321728f5f35ed46becf74315fdc33e21a
am-villanueva <157071990+am-villanueva@users.noreply.github.com>
2024-01-31 05:38:51 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@
             double lengthInKilometers = lengthInMiles * 1.60934;
             Console.WriteLine("Length when converted to Kilometers (km): " + lengthInKilometers);
 
-            Console.Write("Enter Temperature of Fahrenheit (ÃÂÃÂ°F): ");
+            Console.Write("Enter Temperature of Fahrenheit (°F): ");
             double tempInFahrenheit = GetDoubleInput();
             double tempInCelsius = (tempInFahrenheit - 32) * 5 / 9;
-            Console.WriteLine("Temperature when converted to Celsius (ÃÂÃÂ°C): " + tempInCelsius);
+            Console.WriteLine("Temperature when converted to Celsius (°C): " + tempInCelsius);
 
             double[] ages = new double[10];
             double sum = 0;
@@ -131,7 +131,7 @@
 
         static void DisplayFantasyStory()
         {
-            Console.WriteLine(
+            Console.WriteLine( 
 @"Once upon a time in the mystical realm of Toothonia, where tooth fairies thrived and their powers knew no bounds, there
 lived five remarkable tooth fairies, each possessing unique powers, distinct abilities, and magical items. Unlike any
 other tooth fairies who collected children's lost teeth and left a shiny coin or a small treat in return, these five
@@ -139,25 +139,25 @@ fairies, Ari, Phia, Jho, Mader, and Zai used their unique powers, abilities, and
 a truly enchanting experience. As the young dreamers drifted into slumber, they knew that the fairies of Toothonia were 
 watching over them, turning the ordinary act of losing a tooth into an extraordinary journey of dreams and possibilities.
 
-Ari carried a crystal vial filled with Stardust Elixir, which granted children sweet and enchanting dreams
-when sprinkled over their pillows. To illuminate the darkest nights with her radiant wand, creating a soft glow that filled
-children's dreams with wonder.
+Ari carried a crystal vial filled with Stardust Elixir, which granted children sweet and enchanting dreamswhen sprinkled 
+over their pillows. To illuminate the darkest nights with her radiant wand, creating a soft glow that filled children's 
+dreams with wonder.
 
-Phia possessed a satchel of Gossamer Feathers, which, when blown into the wind, would carry children's dreams
-to the farthest corners of Toothonia. She could also communicate with animals, using her 'Whispering Quill' to write secret
+Phia possessed a satchel of Gossamer Feathers, which, when blown into the wind, would carry children's dreams to the 
+farthest corners of Toothonia. She could also communicate with animals, using her 'Whispering Quill' to write secret
 messages and relay children's wishes to their furry and feathered friends.
 
-Jho kept a Palette of Imagination, a magical paint palette that never ran out of colors and allowed her to
-create endless dreamscapes. She could paint the most vivid and fantastical dreams with her 'Prism Prism',
-bringing a burst of color and joy to the sleep of children.
+Jho kept a Palette of Imagination, a magical paint palette that never ran out of colors and allowed her to create endless 
+dreamscapes. She could paint the most vivid and fantastical dreams with her 'Prism Prism', bringing a burst of color and 
+joy to the sleep of children.
 
 Mader treasured a Starry Locket, which contained a piece of the nighttime sky and granted her the ability to navigate 
-through dreams with ease. Mader wove intricate dream catchers with her net that could capture children's fears
-and replace them with dreams of courage and hope.
+through dreams with ease. Mader wove intricate dream catchers with her net that could capture children's fears and replace 
+them with dreams of courage and hope.
 
-Zai held a Vial of Cosmic Dust, a shimmering substance that allowed her to create magical constellations in
-the night sky as a reward for brave children. She shows children glimpses of their future potential through her
-'Cosmic Mirror', inspiring them to chase their dreams."
+Zai held a Vial of Cosmic Dust, a shimmering substance that allowed her to create magical constellations in the night sky
+as a reward for brave children. She shows children glimpses of their future potential through her 'Cosmic Mirror', inspiring 
+them to chase their dreams."
             );
         }
     }
